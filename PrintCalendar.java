010ff76f94c2076823df8e70117f54638d502b41
unknown <bcayuvaraj@gmail.com>
2017-05-24 22:48:20 +0530

    Second

--- PrintCalendar.java
+++ PrintCalendar.java
@@ -122,23 +122,5 @@ import java.util.Scanner;
       return total;
     }
  
-    /** Get the number of days in a month */
- 
-    static int getNumberOfDaysInMonth(int year, int month) {
-      if (month == 1 || month == 3 || month == 5 || month == 7 ||
-        month == 8 || month == 10 || month == 12)
-        return 31;
- 
-      if (month == 4 || month == 6 || month == 9 || month == 11)
-        return 30;
- 
-      if (month == 2) return isLeapYear(year) ? 29 : 28;
- 
-      return 0; // If month is incorrect
-    }
- 
-    /** Determine if it is a leap year */
-    static boolean isLeapYear(int year) {
-      return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
-    }
+    /** Get the number of days in a month *
 }
